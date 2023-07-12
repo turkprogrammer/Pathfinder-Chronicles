9df26b531b86971f059ab0cf6c6ce497b7de383f
Юсупов Роберт <r.yusupov@alean.ru>
2023-07-12 14:10:35 +0500

    RedisAdapter, RedisAdapterTest

--- src/Adapter/RedisAdapter.php
+++ src/Adapter/RedisAdapter.php
@@ -2,7 +2,33 @@
 
 namespace App\Adapter;
 
+use Predis\Client;
+
 class RedisAdapter
 {
+    private Client $redis;
 
+    public function __construct()
+    {
+        // Подключение к Redis или инициализация объекта Predis\Client
+        $this->redis = new Client($_ENV['REDIS_URL']);
+    }
+
+    public function set($key, $value)
+    {
+        // Установка значения по ключу в Redis
+        $this->redis->set($key, $value);
+    }
+
+    public function get($key)
+    {
+        // Получение значения из Redis по ключу
+        return $this->redis->get($key);
+    }
+
+    public function delete($key)
+    {
+        // Удаление значения из Redis по ключу
+        $this->redis->del([$key]);
+    }
 }
--- tests/Adapter/RedisAdapterTest.php
+++ tests/Adapter/RedisAdapterTest.php
@@ -2,9 +2,41 @@
 
 namespace App\Tests\Adapter;
 
+use App\Adapter\RedisAdapter;
 use PHPUnit\Framework\TestCase;
 
 class RedisAdapterTest extends TestCase
 {
+    private RedisAdapter $redisAdapter;
 
+    protected function setUp(): void
+    {
+        // Initialize the RedisAdapter instance
+        $this->redisAdapter = new RedisAdapter();
+    }
+
+    public function testSetAndGet()
+    {
+        // Test the set() method
+        $this->redisAdapter->set('key', 'value');
+
+        // Test the get() method
+        $result = $this->redisAdapter->get('key');
+
+        // Assert that the value retrieved from Redis matches the expected value
+        $this->assertEquals('value', $result);
+    }
+
+    public function testDelete()
+    {
+        // Test the delete() method
+        $this->redisAdapter->set('key', 'value');
+        $this->redisAdapter->delete('key');
+
+        // Test that the value is successfully deleted
+        $result = $this->redisAdapter->get('key');
+
+        // Assert that the value is null after deletion
+        $this->assertNull($result);
+    }
 }
